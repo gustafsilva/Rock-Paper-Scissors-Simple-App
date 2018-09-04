e87c76a6abea60da91a4105c516591d3566ce09c
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-09-03 21:59:24 -0300

    Creating app home screen, where user can navigate between: instructions or play the game

--- src/screens/Welcome/WelcomeScreen.js
+++ src/screens/Welcome/WelcomeScreen.js
@@ -1,25 +1,49 @@
 import React, { Component } from "react";
-import { 
-  View,
-  Text,
-  StyleSheet
-} from "react-native";
+import { View, Image } from "react-native";
+import { H1, Button, Text } from 'native-base';
 
-class WelcomeScreen extends Component {
+import { styles } from './styles';
+
+const imgWelcome = require('../../../assets/img/welcome.png');
+
+/**
+ * Welcome Screen for the App, the first screen the user will see.
+ * From this screen the user can the instructions about the game or can start playing.
+ *
+ * @export
+ * @class WelcomeScreen
+ * @extends {React.Component}
+ */
+export default class WelcomeScreen extends Component {
+  static navigationOptions = {
+    header: null /* remove the header of the screen */
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>WelcomeScreen</Text>
+        <H1>Rock-Paper-Scissors Simple</H1>
+
+        <Image source={imgWelcome} style={styles.image} />
+
+        <View style={styles.buttons}>
+          <Button
+            success
+            onPress={() => this.props.navigation.navigate("Game")}
+            style={styles.button}
+          >
+            <Text>Play Game</Text>
+          </Button>
+
+          <Button
+            info
+            onPress={() => this.props.navigation.navigate("Instructions")}
+            style={styles.button}
+          >
+            <Text>Instructions</Text>
+          </Button>
+        </View>
       </View>
     );
   }
 }
-export default WelcomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
---
+++ src/screens/Welcome/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 150,
+    width: 360
+  },
+  buttons: {
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    margin: 2
+  }
+});
